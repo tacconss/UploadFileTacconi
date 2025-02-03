901594e0b143de62733aa3d1f67816aeadd5120c
Tacconi <tacconisimone@itis-molinari.eu>
2025-02-03 16:50:46 +0100

    sistemazione codice

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 const app = express();
-const multer  = require('multer');
+const multer = require('multer');
+
 var storage = multer.diskStorage({
     destination: function (req, file, callback) {
         callback(null, path.join(__dirname, "files"));
@@ -11,16 +13,30 @@ var storage = multer.diskStorage({
         callback(null, file.originalname);
     }
 });
-const upload = multer({ storage: storage}).single('file');
+
+const upload = multer({ storage: storage }).single('file');
 
 app.use("/", express.static(path.join(__dirname, "public")));
 app.use("/files", express.static(path.join(__dirname, "files")));
+
 app.post('/upload', (req, res) => {
     upload(req, res, (err) => {
+       
+        console.log(req.file.filename);    
+        res.json({ url: "./files/" + req.file.filename });    
+    });
+});
+
+
+app.get('/filelist', (req, res) => {
+    const filesDir = path.join(__dirname, "files");
+    
+    
+    fs.readdir(filesDir, (err, files) => {
         
-    console.log(req.file.filename);    
-    res.json({url: "./files/" + req.file.filename});    
-  })
+        const fileUrls = files.map(file => `./files/${file}`);
+        res.json(fileUrls);
+    });
 });
 
 const server = http.createServer(app);
